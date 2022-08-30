b62b689aa2aa765b07d53caac3d5eb156d9387d1
abrahamzaragoza <abraham.zaragoza@enroutesystems.com>
2022-08-30 13:46:59 -0500

    Updated new_lookup method from Stock model, now it returns a price as a free IEX  user

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,14 @@
 class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-                                  endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:api_key],
+      secret_token: Rails.application.credentials.iex_client[:secret_token],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+
+    response = client.quote(ticker_symbol)
+    response.to_a
+    response["iex_ask_price"]
   end
 end
